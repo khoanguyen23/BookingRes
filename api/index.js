d57b2d308065cbe92743c19db8cd68e3d127bc31
khoanguyen23 <khoahoang151@gmail.com>
2024-05-29 21:51:44 +0700

    [Fix] 500 Error when setting up Swagger

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("./routes/routes");
-const swaggerDocs = require('./swagger'); // Import Swagger configuration
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsdoc = require("swagger-jsdoc");
 
 const app = express();
 const port = 8000;
@@ -13,25 +14,45 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-mongoose.connect(
-  "mongodb+srv://20521328:McvsnJeQrTNWMU7U@bookingres.dprdfjf.mongodb.net/",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-).then(() => {
-  console.log("Connected to MongoDB");
-}).catch((err) => {
-  console.log("Error connecting to MongoDb", err);
-});
+mongoose
+  .connect(
+    "mongodb+srv://20521328:McvsnJeQrTNWMU7U@bookingres.dprdfjf.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDb", err);
+  });
 
 // Swagger setup
 
-
 app.use("/", routes);
-
-swaggerDocs(app);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "My API",
+      version: "1.0.0",
+      description: "A simple Express API",
+    },
+    servers: [
+      {
+        url: "https://booking-res.vercel.app", // URL của server đã triển khai lên Vercel
+      },
+    ],
+  },
+  apis: ["./routes/routes.js"], // Đường dẫn tới file chứa các comment swagger
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
