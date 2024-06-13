1534cbe686ab2a88a393dcb7c1c5df1a2fa24d7e
khoanguyen23 <khoahoang151@gmail.com>
2024-06-13 15:24:59 +0700

    [feat] upload image product modal

--- screens/admin/AddRes.js
+++ screens/admin/AddRes.js
@@ -1,4 +1,13 @@
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  Pressable,
+  Image,
+  ScrollView,
+} from "react-native";
 import React, {
   useState,
   useEffect,
@@ -15,6 +24,9 @@ import BottomSheet from "@gorhom/bottom-sheet";
 import AntDesign from "@expo/vector-icons/AntDesign";
 import Feather from "@expo/vector-icons/Feather";
 import Entypo from "@expo/vector-icons/Entypo";
+import { ActionSheet, Cell } from "@nutui/nutui-react-native";
+import * as ImagePicker from "expo-image-picker";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
 
 const AddRes = () => {
   const bottomSheetRef = useRef(null);
@@ -25,6 +37,8 @@ const AddRes = () => {
   }, []);
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
+  const [modalVisible, setModalVisible] = useState(true);
+  const [images, setImages] = useState([]);
 
   const FooterComponent = ({}) => {
     return (
@@ -44,6 +58,24 @@ const AddRes = () => {
         />
       </View>
     );
+  };
+  const pickImages = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsMultipleSelection: true, // Cho phép chọn nhiều ảnh
+        quality: 1,
+      });
+
+      if (!result.canceled) {
+        setImages(result.assets.map((asset) => asset.uri));
+      }
+    } catch (error) {
+      console.log("Error picking images:", error);
+    }
+  };
+  const removeImage = (index) => {
+    setImages((prevImages) => prevImages.filter((_, i) => i !== index));
   };
 
   const SearchIcon = () => (
@@ -94,11 +126,23 @@ const AddRes = () => {
               <AntDesign name="right" size={24} color="#B4B4BC" />
             </View>
           </View>
-          <View className="p-4">
+          <View className="p-4 mt-2">
             <Text className="text-base">Địa chỉ nhà hàng : </Text>
             <Text className="text-base mt-2">Longitude: {longitude}</Text>
             <Text className="text-base mt-2">Latitude: {latitude}</Text>
           </View>
+          <Pressable
+            style={[styles.button, styles.buttonOpen]}
+            onPress={() => setModalVisible(true)}
+          >
+            <Text style={styles.textStyle}>Show Modal</Text>
+          </Pressable>
+          {/* <TouchableOpacity className="bg-slate-100 p-3 m-3 flex flex-row justify-between rounded">
+            <Text className="text-base font-bold text-emerald-500">
+              Tiếp theo
+            </Text>
+            <AntDesign name="arrowright" size={24} color="#21BF73" />
+          </TouchableOpacity> */}
           <View style={{ flex: 1 }}>
             <GooglePlacesAutocomplete
               onPress={(data, details = null) => {
@@ -145,6 +189,146 @@ const AddRes = () => {
               )}
             />
           </View>
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible(!modalVisible);
+            }}
+          >
+            <ScrollView style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text className="text-xl mb-4">Thêm sản phẩm</Text>
+                <TouchableOpacity
+                  onPress={pickImages}
+                  className="border-dashed border-2 border-indigo-600 p-2"
+                >
+                  <Image
+                    style={styles.tinyLogo}
+                    source={{
+                      uri:
+                        images.length > 0
+                          ? images[0]
+                          : "https://t4.ftcdn.net/jpg/04/81/13/43/360_F_481134373_0W4kg2yKeBRHNEklk4F9UXtGHdub3tYk.jpg",
+                    }}
+                  />
+                </TouchableOpacity>
+                {/* <TouchableOpacity
+                    style={styles.removeLargeIconContainer}
+                    onPress={() => removeImage(index + 1)}
+                  >
+                    <FontAwesome6 name="xmark" size={16} color="white" />
+                  </TouchableOpacity> */}
+                {/* <View className="flex flex-row space-x-2">
+                  <View className="mt-2">
+                    <Image
+                      className="w-12 h-12 rounded-md"
+                      source={{
+                        uri: "https://thanhnienmoi.com/upload/images/top-5-dia-diem-du-lich-o-thuy-si-03.jpg",
+                      }}
+                    />
+                  </View>
+                  <View className="mt-2">
+                    <Image
+                      className="w-12 h-12 rounded-md"
+                      source={{
+                        uri: "https://thanhnienmoi.com/upload/images/top-5-dia-diem-du-lich-o-thuy-si-03.jpg",
+                      }}
+                    />
+                  </View>
+                </View> */}
+                <View style={styles.imageList}>
+                  {images.slice(1).map((imageUri, index) => (
+                    <View key={index} style={styles.imageWrapper}>
+                      <Image
+                        style={styles.smallImage}
+                        source={{ uri: imageUri }}
+                      />
+                      <TouchableOpacity
+                        style={styles.removeIconContainer}
+                        onPress={() => removeImage(index + 1)}
+                      >
+                        <FontAwesome6 name="xmark" size={16} color="white" />
+                      </TouchableOpacity>
+                    </View>
+                  ))}
+                </View>
+
+                <View className="mt-10" style={{}}>
+                  <Text>Phần ở dưới</Text>
+                  <View className="flex flex-row justify-between">
+                   <View className="w-36">
+                   <TextInput
+                    value={latitude}
+                      mode="outlined"
+                      label="Outlined input"
+                      placeholder="Type something"
+                    />
+                   </View>
+                   <View className="w-36">
+                   <TextInput
+                      mode="outlined"
+                      label="Outlined input"
+                      placeholder="Type something"
+                    />
+                   </View>
+                  </View>
+
+                  <View className="space-y-4 grid">
+                    <TextInput
+                      mode="outlined"
+                      label="Outlined input"
+                      placeholder="Type something"
+                    />
+                    <TextInput
+                      mode="outlined"
+                      label="Outlined input"
+                      placeholder="Type something"
+                    />
+                    <TextInput
+                      mode="outlined"
+                      label="Outlined input"
+                      placeholder="Type something"
+                    />
+                    <TextInput
+                      mode="outlined"
+                      label="Outlined input"
+                      placeholder="Type something"
+                    />
+                    <TextInput
+                      mode="outlined"
+                      label="Outlined input"
+                      placeholder="Type something"
+                    />
+                    <TextInput
+                      mode="outlined"
+                      label="Outlined input"
+                      placeholder="Type something"
+                    />
+                    <TextInput
+                      mode="outlined"
+                      label="Outlined input"
+                      placeholder="Type something"
+                    />
+                    <TextInput
+                      mode="outlined"
+                      label="Outlined input"
+                      placeholder="Type something"
+                    />
+                  </View>
+                </View>
+
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={styles.textStyle}>Hide Modal</Text>
+                </Pressable>
+              </View>
+            </ScrollView>
+          </Modal>
         </View>
       </BottomSheet>
     </View>
@@ -159,6 +343,43 @@ const styles = StyleSheet.create({
     backgroundColor: "#FFFFFF",
     justifyContent: "center",
     alignItems: "center",
+  },
+  modalView: {
+    margin: 10,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    // alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    marginTop: 35,
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  tinyLogo: {
+    width: "100%",
+    height: 250,
+    objectFit: "cover",
   },
   map: {
     flex: 1,
@@ -185,5 +406,42 @@ const styles = StyleSheet.create({
   sheetHandleIndicator: {
     backgroundColor: "#000",
     width: 40,
+  },
+  imageList: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  imageWrapper: {
+    margin: 5,
+    position: "relative",
+  },
+  smallImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 10,
+  },
+  removeIconContainer: {
+    position: "absolute",
+    top: -4,
+    left: -7,
+    backgroundColor: "red",
+    borderRadius: 50,
+    width: 15,
+    height: 15,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  removeLargeIconContainer: {
+    position: "absolute",
+    top: 70,
+    left: 35,
+    backgroundColor: "red",
+    borderRadius: 50,
+    width: 25,
+    height: 25,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
