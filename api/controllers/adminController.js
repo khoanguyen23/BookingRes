641ad4829547644ae112351a1449a6f8006441eb
khoanguyen23 <khoahoang151@gmail.com>
2024-06-22 21:34:11 +0700

    [fix] delete user from admin

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -15,10 +15,14 @@ const adminController = {
   deleteUser: async (req, res) => {
     try {
       const user = await User.findById(req.params.userId);
-      // const user = await User.findByIdAndDelete(req.params.userId);
+  
       if (!user) {
-        return res.status(404).json({ message: "không tìm thấy người dùng" });
+        return res.status(404).json({ message: "Không tìm thấy người dùng" });
       }
+  
+      // Delete the user from the database
+      await User.findByIdAndDelete(req.params.userId);
+  
       res.status(200).json({ message: "Xoá thành công", deletedUser: user });
     } catch (err) {
       res.status(500).json(err);
