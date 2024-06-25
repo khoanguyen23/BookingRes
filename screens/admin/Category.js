112a25e2c4f9b09bdbce151ea07830a1ed1fe397
khoanguyen23 <khoahoang151@gmail.com>
2024-06-26 00:22:25 +0700

    [feat] add loading state

--- screens/admin/Category.js
+++ screens/admin/Category.js
@@ -5,13 +5,15 @@ import {
   ScrollView,
   TouchableOpacity,
   StyleSheet,
+  ActivityIndicator,
 } from "react-native";
 import { API_URL } from "@env";
 import { Avatar } from "@rneui/themed";
 
 const Category = () => {
   const [categories, setCategories] = useState([]);
-
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   useEffect(() => {
     fetchCategories();
   }, []);
@@ -21,16 +23,45 @@ const Category = () => {
       const response = await fetch(`${API_URL}/categories`);
       const data = await response.json();
       setCategories(data);
+      setLoading(false);
       console.log("categories:", data);
     } catch (error) {
-      console.error("Error fetching categories:", error);
+      setError("Error fetching categories");
+      setLoading(false);
     }
+  }
+
+  if (loading) {
+    return (
+      <View style={styles.center}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Loading categories...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.center}>
+        <Text>{error}</Text>
+        <Button
+          buttonStyle={styles.retryButton}
+          onPress={fetchAllRestaurants}
+          title="Retry"
+        />
+      </View>
+    );
   }
 
   const renderCategoryItem = (item) => (
     <TouchableOpacity key={item.id}>
       <View className="w-[120] h-[140] bg-white items-center justify-center border border-[#DDDDDD] rounded-lg mt-2">
-        <Avatar avatarStyle={{objectFit:"cover"}} size={100} rounded source={{ uri: item.image }} />
+        <Avatar
+          avatarStyle={{ objectFit: "cover" }}
+          size={100}
+          rounded
+          source={{ uri: item.image }}
+        />
         <Text>{item.name}</Text>
       </View>
     </TouchableOpacity>
@@ -46,3 +77,15 @@ const Category = () => {
 };
 
 export default Category;
+
+const styles = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  retryButton: {
+    marginTop: 10,
+    backgroundColor: "#FF6347",
+  },
+});
