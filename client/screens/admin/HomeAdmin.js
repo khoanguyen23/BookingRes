dc63be5607bc105a61c551d289f17f689c497d5d
khoanguyen23 <khoahoang151@gmail.com>
2024-07-06 02:47:40 +0700

    [feat] refactor call address

--- client/screens/admin/HomeAdmin.js
+++ client/screens/admin/HomeAdmin.js
@@ -1,6 +1,6 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ScrollView, Text, View } from "react-native";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState, useCallback } from "react";
 import { useNavigation } from "@react-navigation/native";
 import HomeAdminCard from "../../components/HomeAdminCard";
 import { UserType } from "../../UserContext";
@@ -8,7 +8,6 @@ import { API_URL } from "@env";
 import Entypo from "@expo/vector-icons/Entypo";
 import { LinearGradient } from "expo-linear-gradient";
 import { Avatar } from "@rneui/themed";
-import { StyleSheet } from "react-native";
 import { LineChart } from "react-native-gifted-charts";
 import jwt_decode from "jwt-decode";
 import axios from "axios";
@@ -20,26 +19,15 @@ const HomeAdmin = () => {
   const [info, setInfo] = useState([]);
   const [error, setError] = useState(null);
 
-  console.log("User ID:", userId); // Add a label to the console log for clarity
+  console.log("User ID:", userId);
   console.log("User:", user);
 
-  // useEffect(() => {
-  //   getUser();
-  // }, []);
-
-  // async function getUser() {
-  //   try {
-  //     const response = await fetch(`${API_URL}/address/${userId}`);
-  //     const data = await response.json();
-  //     setInfo(data);
-  //   } catch (error) {
-  //     setError("Error fetching categories");
-  //   }
-  // }
-
-  const fetchAddress = async () => {
+  const fetchAddress = useCallback(async () => {
     try {
       const token = await AsyncStorage.getItem("authToken");
+      if (!token) {
+        throw new Error("Token not found");
+      }
       const decodedToken = jwt_decode(token);
       const userId = decodedToken.userId;
       setUserId(userId);
@@ -47,10 +35,11 @@ const HomeAdmin = () => {
     } catch (error) {
       console.log("Error fetching address", error);
     }
-  };
+  }, [setUserId]);
+
   useEffect(() => {
     fetchAddress();
-  }, []);
+  }, [fetchAddress]);
 
   const fetchAddressData = async (userId) => {
     try {
@@ -63,33 +52,35 @@ const HomeAdmin = () => {
       console.log("Error fetching address data", error);
     }
   };
+
   const data = [
-    { value: 300, label: "M" }, // Monday
-    { value: 150, label: "T" }, // Tuesday
-    { value: 200, label: "W" }, // Wednesday
-    { value: 250, label: "T" }, // Thursday
-    { value: 300, label: "F" }, // Friday
-    { value: 450, label: "S" }, // Saturday
-    { value: 350, label: "S" }, // Sunday
+    { value: 300, label: "M" },
+    { value: 150, label: "T" },
+    { value: 200, label: "W" },
+    { value: 250, label: "T" },
+    { value: 300, label: "F" },
+    { value: 450, label: "S" },
+    { value: 350, label: "S" },
   ];
+
   const lineData = [
-    { value: 5, dataPointText: "5" }, // Thứ 2
-    { value: 12, dataPointText: "12" }, // Thứ 3
-    { value: 45, dataPointText: "45" }, // Thứ 4
-    { value: 35, dataPointText: "35" }, // Thứ 5
-    { value: 62, dataPointText: "62" }, // Thứ 6
-    { value: 90, dataPointText: "90" }, // Thứ 7
-    { value: 100, dataPointText: "100" }, // Chủ nhật
+    { value: 5, dataPointText: "5" },
+    { value: 12, dataPointText: "12" },
+    { value: 45, dataPointText: "45" },
+    { value: 35, dataPointText: "35" },
+    { value: 62, dataPointText: "62" },
+    { value: 90, dataPointText: "90" },
+    { value: 100, dataPointText: "100" },
   ];
 
   const lineData2 = [
-    { value: 20, dataPointText: "20" }, // Thứ 2 tuần trước
-    { value: 35, dataPointText: "35" }, // Thứ 3 tuần trước
-    { value: 70, dataPointText: "70" }, // Thứ 4 tuần trước
-    { value: 12, dataPointText: "12" }, // Thứ 5 tuần trước
-    { value: 37, dataPointText: "37" }, // Thứ 6 tuần trước
-    { value: 55, dataPointText: "55" }, // Thứ 7 tuần trước
-    { value: 90, dataPointText: "90" }, // Chủ nhật tuần trước
+    { value: 20, dataPointText: "20" },
+    { value: 35, dataPointText: "35" },
+    { value: 70, dataPointText: "70" },
+    { value: 12, dataPointText: "12" },
+    { value: 37, dataPointText: "37" },
+    { value: 55, dataPointText: "55" },
+    { value: 90, dataPointText: "90" },
   ];
 
   return (
@@ -109,7 +100,7 @@ const HomeAdmin = () => {
               </Text>
               <Text className="text-xl">Welcome to Dashboard</Text>
             </View>
-            <Avatar size={70} rounded source={{ uri: info.avatar }} />
+            <Avatar size={70} rounded source={{ uri: user.avatar }} />
           </View>
           <View
             style={{
@@ -149,12 +140,11 @@ const HomeAdmin = () => {
               startFillColor="transparent"
               endFillColor="transparent"
               noOfSections={5}
-              maxValue={150} // Giá trị lớn nhất của y-axis, bạn có thể điều chỉnh
+              maxValue={150}
               yAxisThickness={0}
               xAxisThickness={0}
               yAxisTextStyle={{ color: "grey", fontSize: 12 }}
               xAxisTextStyle={{ color: "grey", fontSize: 12 }}
-              // yAxisLabelTexts={["0", "25", "50", "75", "100", "125", "150"]} // Các giá trị trên y-axis
               xAxisLabelTexts={[
                 "Mon",
                 "Tue",
@@ -163,37 +153,11 @@ const HomeAdmin = () => {
                 "Fri",
                 "Sat",
                 "Sun",
-              ]} // Các ngày trong tuần
+              ]}
               onDataPointClick={(item, index) => {
                 alert(`Value: ${item.value}, Additional Text: additional text`);
               }}
               initialSpacing={20}
-
-              // focusEnabled={true}
-              // showStripOnFocus={true}
-              // showTextOnFocus={true}
-              // focusedDataPointShape="circle"
-              // focusedDataPointWidth={10}
-              // focusedDataPointHeight={10}
-              // focusedDataPointColor="#ff6347" // Ví dụ, màu cam đậm
-              // focusedDataPointRadius={5}
-              // onFocus={(item, index) => {
-              //   console.log(`Focused on data point at index ${index} with value ${item.value}`);
-              // }}
-              // focusedCustomDataPoint={(item, index) => (
-              //   <View
-              //     style={{
-              //       width: 20,
-              //       height: 20,
-              //       borderRadius: 10,
-              //       backgroundColor: 'blue',
-              //       justifyContent: 'center',
-              //       alignItems: 'center',
-              //     }}
-              //   >
-              //     <Text style={{ color: 'white' }}>{item.value}</Text>
-              //   </View>
-              // )}
             />
           </View>
           <View className="flex flex-row flex-wrap justify-around mb-10">
@@ -232,7 +196,6 @@ const HomeAdmin = () => {
               bgColor="#E1E2FE"
               onPress={() => navigation.navigate("Category")}
             />
-
             <HomeAdminCard
               title="Notification"
               iconUri="https://res.cloudinary.com/dc5xcbmvp/image/upload/v1719304124/icons8-bell-100_qslekd.png"
