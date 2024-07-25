41b35859d17cfe59f7e6f9635a26c688716e469f
khoanguyen23 <khoahoang151@gmail.com>
2024-07-25 22:27:36 +0700

    [feat] add resdetail in admin

---
+++ client/screens/admin/ResDetail.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ResDetail = () => {
+  return (
+    <View>
+      <Text>ResDetail</Text>
+    </View>
+  )
+}
+
+export default ResDetail
